09069cf6fe6d0d79602c7c582ba7c500c2b02710
boynuibon3s <boynuibon3s@gmail.com>
2022-01-21 14:46:44 +0700

    unknown number

--- B14/b14-unknown_number/js/function.js
+++ B14/b14-unknown_number/js/function.js
@@ -15,6 +15,11 @@ const LOCAL = "game_save_list";
 const number_of_predict_button = 25;
 const life_count = 3;
 
+const random_number = (random) => {
+    let randomNumber = Math.floor(Math.random() * random) + 1;
+    return randomNumber;
+}
+
 const setLocalStorage = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
 }
@@ -23,127 +28,140 @@ const setLocalStorage = (key, value) => {
 const getLocalStorage = (key) => {
 
     let data = localStorage.getItem(key);
-    data = data ? JSON.parse(data): []
+    data = data ? JSON.parse(data) : []
     return data
 }
 
+//list of button
 const button_list = (number) => {
     let str = ""
     for (let i = 0; i < number; i++) {
-            
-          str +=`<button class="btn-number hint-number" value="${i+1}">${i+1}</button>`
+
+        str += `<button class="btn-number hint-number" value="${i + 1}">${i + 1}</button>`
     }
     btn_number_list.html(str);
 }
 
+//health
 const life = (number_of_life) => {
     let str = ""
     for (let i = 0; i < number_of_life; i++) {
-            
-          str +=`<div class="game-life-try margin-right-10"></div>`
+
+        str += `<div class="game-life-try margin-right-10"></div>`
     }
     game_life.html(str);
 }
 
-const gameLifeCount = (number_of_count)=>{
-    
+//add the guess bar
+const gameLifeCount = (number_of_count) => {
+
     $('.game-life-try').eq(number_of_count).addClass("count");
 }
 
-const gamePredictList = (val, secret_number, num_of_guess)=>{
-    let str=""
-    if (val>secret_number) {
+/* //game show content without local storage
+const gamePredictList = (val, secret_number, num_of_guess) => {
+    let str = ""
+    if (val > secret_number) {
         console.log(">");
-        str+=`<li>
+        str += `<li>
         <div class="game-notice wrong">Wrong!</div>
-        <div>Try ${num_of_guess+1}: <span class="game-guess">${val}</span>, you guess it wrong. Your number is too high.</div>
+        <div>Try ${num_of_guess + 1}: <span class="game-guess">${val}</span>, you guess it wrong. Your number is too high.</div>
     </li>`
-        
-    } else if (val<secret_number) {
+
+    } else if (val < secret_number) {
         console.log("<");
-        str+=`<li>
+        str += `<li>
         <div class="game-notice wrong">Wrong!</div>
-        <div>Try ${num_of_guess+1}: <span class="game-guess">${val}</span>, you guess it wrong. Your number is too low.</div>
+        <div>Try ${num_of_guess + 1}: <span class="game-guess">${val}</span>, you guess it wrong. Your number is too low.</div>
     </li>`
-    } else if (val==secret_number) {
+    } else if (val == secret_number) {
         console.log("=");
-        str+=`<li>
+        str += `<li>
         <div class="game-notice true">You have win!</div>
-        <div>Try ${num_of_guess+1}: <span class="game-guess">${val}</span>, Congrats!! you guess it right!!</div>
+        <div>Try ${num_of_guess + 1}: <span class="game-guess">${val}</span>, Congrats!! you guess it right!!</div>
     </li>`
     }
     game_content_predict.html(str)
-
 }
+*/
 
-const getGameList = (data)=>{
-    let str=""
-    data.forEach((element) =>{
-        
-        if (element.value>element.secret) {
-            
-            str+=`<li>
+//get content, for local storage
+const getGameList = (data) => {
+    let str = ""
+    data.forEach((element) => {
+
+        if (element.value > element.secret) {
+
+            str += `<li>
             <div class="game-notice wrong">Wrong!</div>
-            <div>Try ${element.guess+1}: <span class="game-guess">${element.value}</span>, you guess it wrong. Your number is too high.</div>
+            <div>Try ${element.guess + 1}: <span class="game-guess">${element.value}</span>, you guess it wrong. Your number is too high.</div>
         </li>`
-            
-        } else if (element.value<element.secret) {
-            
-            str+=`<li>
+
+        } else if (element.value < element.secret) {
+
+            str += `<li>
             <div class="game-notice wrong">Wrong!</div>
-            <div>Try ${element.guess+1}: <span class="game-guess">${element.value}</span>, you guess it wrong. Your number is too low.</div>
+            <div>Try ${element.guess + 1}: <span class="game-guess">${element.value}</span>, you guess it wrong. Your number is too low.</div>
         </li>`
-        } else if (element.value==element.secret) {
-            
-            str+=`<li>
-            <div class="game-notice true">You have win!</div>
-            <div>Try ${element.guess+1}: <span class="game-guess">${element.value}</span>, Congrats!! you guess it right!!</div>
+        } else if (element.value == element.secret) {
+
+            str += `<li>
+            <div class="game-notice true">You win!</div>
+            <div>Try ${element.guess + 1}: <span class="game-guess">${element.value}</span>, Congrats!! you guess it right!!</div>
         </li>`
         }
-        
+
         gameLifeCount(element.guess)
         game_predict_number.html(element.value)
+        $('.btn-number').eq(element.value-1).css('box-shadow', '0px 0px 18px rgb(95, 84, 160)');
     });
     game_content_predict.html(str)
-    
-    
-
 }
 
-const setGameWin = (guess,secret_number)=>{
-    let str=""
-        str+=`<div class="game-result">
-        Secret number is: <span class="game-notice hint">${secret_number}</span> , you did it in <span>${guess+1}</span> try!!
+
+const setGameWin = (guess, secret_number) => {
+    let str = ""
+    str += `<div class="game-result">
+        Secret number is: <span class="game-notice hint">${secret_number}</span> , you did it in <span>${guess + 1}</span> try!!
         </div>`
     game_content_predict.append(str)
     setGameOver();
 }
 
-const setGameLose = (secret_number)=>{
-    let str=""
-    str+=`<div class="game-result">
+const setGameLose = (secret_number) => {
+    let str = ""
+    str += `<div class="game-result">
             Secret number is: <span class="game-notice hint">${secret_number}</span> , you lose the game, try again!!
             </div>`
     game_content_predict.append(str)
     setGameOver();
 }
 
-const random_number = (random)=>{
-    let randomNumber = Math.floor(Math.random() * random) + 1;
-    return randomNumber;
-}
-
 const setGameOver = () => {
     $('.btn-number').removeClass("hint-number")
     $('.btn-number').prop('disabled', true);
+    $('.btn-hint').prop('disabled', true);
+}
+
+const setGameHint = (value, secret_number, number_of_predict_button )=>{
+    
+    console.log("hint value",value);
+
+    if (value > secret_number) {
+        for (let i = value - 1; i <= number_of_predict_button; i++) {
+            $(".hint-number").eq(i).prop("disabled", true);
+        }
+    } else if (value < secret_number) {
+        for (let i = 0; i < value; i++) {
+            $(".hint-number").eq(i).prop("disabled", true);
+        }
+    }
 }
 
 const setResetGame = () => {
-    
-    game_predict_number.html("") ;
+    game_predict_number.html("");
     game_content_predict.html("");
-    $('.game-life-try').removeClass("count");
+$('.game-life-try').removeClass("count");
     $('.btn-number').prop('disabled', false);
-    
-    
+    $('.btn-hint').prop('disabled', false);
 }
--- B14/b14-unknown_number/js/main.js
+++ B14/b14-unknown_number/js/main.js
@@ -1,95 +1,102 @@
-
-
-
-
 $(document).ready(function () {
-
-    
-    let data = getLocalStorage(LOCAL); 
+    let data = getLocalStorage(LOCAL);
 
     let secret_number = random_number(25);
-    let guess = 0
-    
+    let guess = 0;
+
     button_list(number_of_predict_button);
-    life(life_count)
+    life(life_count);
 
-    getGameList(data)
-    if(data.length > 0){
-        guess = data[data.length-1].guess+1
-        secret_number = data[data.length-1].secret
-    }
-    console.log("load", data);
-    console.log("secret",secret_number);
-    console.log("gues",guess);
-
-    $('.btn-number').click(function (e) { 
-        e.preventDefault();
-        let val=$(this).val();
-        
-        console.log(val);
-        game_predict_number.html(val)
-            
-        let obj = {
-                guess: guess,
-                value : val,
-                secret : secret_number, 
-            }
-        
-        
-        data.push(obj);
-        
-        console.log("data:",data);
-        
-        if(guess<life_count ){
-            getGameList(data)
-            
-            console.log("try:",guess);
-            guess++;
-            console.log("try2:",guess);
-            if(obj.value == obj.secret)
-            {
-                setGameWin( obj.guess,  obj.secret);
-                
-            }
-        } 
-        if( guess>=life_count &&  obj.value!= obj.secret){
-            setGameLose(obj.secret)
-        }
-        
-
-    });
     
-        
-    btn_restart.click(function (e) { 
+
+    getGameList(data);
+    if (data.length > 0) {
+        guess = data[data.length - 1].guess + 1;
+        secret_number = data[data.length - 1].secret;
+
+        if (data[data.length - 1].value == data[data.length - 1].secret)
+        {
+            setGameWin(guess-1, secret_number);
+        }
+    }
+
+    if (data.length > life_count-1 && data[data.length - 1].value != data[data.length - 1].secret) 
+    {
+        setGameLose(secret_number);
+    }
+    
+    console.log("load", data);
+    console.log("secret", secret_number);
+    console.log("gues", guess);
+
+    //number button
+    $(".btn-number").click(function (e) {
+        e.preventDefault();
+        let val = $(this).val();
+
+        console.log(val);
+        game_predict_number.html(val);
+
+        let obj = {
+            guess: guess,
+            value: val,
+            secret: secret_number,
+        };
+
+        data.push(obj);
+
+        console.log("data:", data);
+
+        if (guess < life_count) {
+            getGameList(data);
+
+            console.log("try:", guess);
+            guess++;
+            console.log("try2:", guess);
+            if (obj.value == obj.secret) {
+                setGameWin(obj.guess, obj.secret);
+            }
+        }
+        if (guess >= life_count && obj.value != obj.secret) {
+            setGameLose(obj.secret);
+        }
+    });
+
+    //play again  button
+    btn_restart.click(function (e) {
         e.preventDefault();
         setResetGame();
         secret_number = random_number(25);
-        console.log("secret",secret_number);
-        data = []
+        console.log("secret", secret_number);
+        data = [];
         setLocalStorage(LOCAL, data);
-        guess = 0
-    }); 
+        guess = 0;
+        $('.btn-number').css('box-shadow',"")
+        alert("Restart the game!")
+    });
 
-    btn_hint.click(function (e) { 
+    //hint button
+    btn_hint.click(function (e) {
         e.preventDefault();
-        let val = game_predict_number.html()
-    
-        console.log(val);
-        if (val>secret_number){
-            for (let i = val-1; i <= number_of_predict_button ; i++) {
-                $('.hint-number').eq(i).prop('disabled', true);   
+        let val = game_predict_number.html();
+
+        console.log("hint value",val);
+        if (val > secret_number) {
+            for (let i = val - 1; i <= number_of_predict_button; i++) {
+                $(".hint-number").eq(i).prop("disabled", true);
             }
-        } else if (val<secret_number){
-            for (let i = 0; i < val ; i++) {
-                $('.hint-number').eq(i).prop('disabled', true);   
+        } else if (val < secret_number) {
+            for (let i = 0; i < val; i++) {
+                $(".hint-number").eq(i).prop("disabled", true);
             }
         }
     });
 
-
-    btn_save.click(function (e) { 
+    //save button
+    btn_save.click(function (e) {
         e.preventDefault();
-        console.log("save game:",data);
+        console.log("save game:", data);
         setLocalStorage(LOCAL, data);
+        alert("Save success!")
     });
-}); 
+});
